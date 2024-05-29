25cd955be0e93ee39183109edf319fefcb0a7cc8
dvachette <donatien.vachette@hotmail.com>
2024-05-29 09:24:30 +0200

    mod __init__ to auto install modules

--- software/pygame_gui/__init__.py
+++ software/pygame_gui/__init__.py
@@ -1,6 +1,22 @@
+import subprocess
+AUTHORS = ["Donatien Vachette"]
+NAME = "pygame-gui"
+required = [
+    "pygame",
+    "pygame-vkeyboard",
+]
+__installed_modules = subprocess.run("pip freeze", shell=True, capture_output=True).stdout.decode('utf-8').lower()
+for mod in required:
+    if mod not in __installed_modules:
+        print(mod,"is not installed")
+        subprocess.run("pip install {}".format(mod),shell = True)
+
+del __installed_modules
 from .main import *
 import pygame_vkeyboard
 import pygame
-# Initialisation
+
+
 if not pygame.get_init():
     pygame.init()
+print(f'Module {NAME} by {", ".join(AUTHORS)}.')
